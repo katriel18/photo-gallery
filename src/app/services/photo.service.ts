ebf7e2ad9a6ad1a642ac69736e22065dfd89486d
katriel <katriel181920@gmail.com>
2020-10-02 17:36:45 -0500

    Carga de fotos desde el sistema de archivos

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -12,6 +12,8 @@ export class PhotoService {
 
   public photos: Photo[] = [];
 
+  private PHOTO_STORAGE: string = "photos";
+
   constructor() { }
 
   public async addNewToGallery() {
@@ -25,6 +27,12 @@ export class PhotoService {
     // Save the picture and add it to photo collection
     const savedImageFile = await this.savePicture(capturedPhoto);
     this.photos.unshift(savedImageFile);
+
+    Storage.set({
+      key: this.PHOTO_STORAGE,
+      value: JSON.stringify(this.photos)
+    });
+
   }
 
   private async savePicture(cameraPhoto: CameraPhoto) {
@@ -63,6 +71,25 @@ export class PhotoService {
     };
     reader.readAsDataURL(blob);
   });
+
+  public async loadSaved() {
+    // Retrieve cached photo array data
+    const photoList = await Storage.get({ key: this.PHOTO_STORAGE });
+    this.photos = JSON.parse(photoList.value) || [];
+  
+    // Display the photo by reading into base64 format
+    for (let photo of this.photos) {
+      // Read each saved photo's data from the Filesystem
+      const readFile = await Filesystem.readFile({
+          path: photo.filepath,
+          directory: FilesystemDirectory.Data
+      });
+
+      // Web platform only: Load the photo as base64 data
+      photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
+    }
+
+  }
 
 }
 
