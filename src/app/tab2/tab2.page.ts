24484104ff43bf40517201e0d41e5d4bea215390
katriel <katriel181920@gmail.com>
2020-10-02 19:57:47 -0500

    Eliminación de fotos

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
-import { PhotoService } from '../services/photo.service';
+import {Photo,PhotoService } from '../services/photo.service';
+
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -13,10 +15,35 @@ export class Tab2Page {
     await this.photoService.loadSaved();
   }
 
-  constructor(public photoService: PhotoService) { }
+  constructor(
+    public photoService: PhotoService,
+    public actionSheetController: ActionSheetController
+  ) { }
 
   addPhotoToGallery() {
     this.photoService.addNewToGallery();
+  }
+
+  public async showActionSheet(photo: Photo, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+          }
+      }]
+    });
+    await actionSheet.present();
   }
 
 }
